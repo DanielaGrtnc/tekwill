69a041ccdf48efe33a83f8b4a460406c40ffb318
DanielaGrtnc <gritenco.daniela@mail.ru>
2024-02-21 20:42:02 +0200

    Homework js

---
+++ landing-page/homework.js
@@ -0,0 +1,19 @@
+// 1 Punct Homework
+
+const country = 'Cuba';
+const continent = 'North America';
+const population = '11.2';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// 2 Punct Homework
+
+const isIsland = true;
+console.log(isIsland);
+
+// 3 Punct Homework
+
+const description = `${country} is in ${continent}, and it has ${population} million people.`;
+console.log(description);
